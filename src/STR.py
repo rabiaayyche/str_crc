94f30a4b3200c03ddd0ee3c1809cebe3d81ca30a
rabiaayyche <rabia.ayche.serradj@mni.thm.de>
2024-03-28 10:52:46 +0100

    Update STR.py
    Added documentation

--- src/STR.py
+++ src/STR.py
@@ -20,6 +20,12 @@ def create_coad_psi_data():
 
 
 def create_STR_bed(str_panel):
+    """ 
+    This method creat a .bed file for the TRAL STR panel.
+
+    Args:
+        str_panel (str): Path to TRAL STR panel.
+    """
     str_full_annotatted = pd.read_csv(str_panel, header=None, sep="\t")
     str_full_annotatted = str_full_annotatted.iloc[:, [0,1,2,3,5,11]]
     str_full_annotatted = str_full_annotatted.rename(columns={0: "chr", 1: "start", 2: "end", 3: "period", 5: "msa", 11: "gene_names"})
@@ -43,6 +49,15 @@ def create_STR_bed(str_panel):
 #liftOver -bedPlus=3 data/str_full_annotated_bed.bed data/hg38ToHg19.over.chain.gz data/str_full_Hg38ToHg19.bed data/unmapped.bed
 
 def process_liftOver(liftOver_data):
+    """ 
+    This method process the liftOver output.
+
+    Args:
+        liftOver_data (str): Path to STR liftOver output -> rename gene_name column and delete strand column.
+
+    Returns:
+        pandas dataframe: processed STR liftOver panel.
+    """
     str_liftover = pd.read_csv(liftOver_data, sep='[\t;]', names=['chr', 'start', 'end', 'msa', 'gene_name', 'period', 'strand'])
     str_liftover = str_liftover.rename(columns={"gene_name":"GeneSymbol"})
     del str_liftover['strand']
@@ -50,6 +65,18 @@ def process_liftOver(liftOver_data):
 
 
 def combine_STR_splice_data(str_liftover, crc_psi_splAdder, crc_psi_spliceseq):
+    """
+    This method combines the STR liftOver panel and the ASE from both databases (SplAdder & SpliceSeq) based on the column "GeneSymbol".
+    This makes it possible to determine in which genes STR and ASE occur.
+
+    Args:
+        str_liftover (pandas dataframe): STR liftOver panel.
+        crc_psi_splAdder (pandas dataframe): ASE from splAdder database.
+        crc_psi_spliceseq (pandas dataframe): ASE from SpliceSeq database.
+
+    Returns:
+        pandas dataframe: merged dataframe of STR liftOver panel with both databases.
+    """
     merged_df_splAdder = pd.merge(str_liftover, crc_psi_splAdder, on='GeneSymbol', how='inner')
     merged_df_splAdder = merged_df_splAdder.drop_duplicates()
     merged_df_splAdder = merged_df_splAdder.drop_duplicates(subset=['start', 'end'], keep=False)
@@ -63,45 +90,56 @@ def combine_STR_splice_data(str_liftover, crc_psi_splAdder, crc_psi_spliceseq):
 
 
 def compare_spliSeq_splAdder(merged_df_splAdder, merged_df_spliceSeq, criterion1, criterion2=None):
-    # splice type und gene type darstellen
+    """
+    This method compared the combined dataframes from method "def combine_STR_splice_data(str_liftover, crc_psi_splAdder, crc_psi_spliceseq)"
+    If only criterion 1 is specified, the 10 most frequently occurring ones are displayed visually for each database. 
+    If both criteria are specified, the number of ASEs is displayed.
+
+    Args:
+        merged_df_splAdder (pandas dataframe): ASE (splAdder database) and STR liftOver combined dataframe.
+        merged_df_spliceSeq (pandas dataframe): ASE (spliceSeq database)cand STR liftOver combined dataframe.
+        criterion1 (str): Criterion to compare both databases.
+        criterion2 (str, optional): Criterion to compare both databases. Defaults to None.
+    """
+    
     if criterion2 == None:
         gene_counts_spliceSeq = merged_df_spliceSeq.groupby(criterion1).size().sort_values(ascending=False)
         gene_counts_splAdder = merged_df_splAdder.groupby(criterion1).size().sort_values(ascending=False)
+        
+        # Select the 10 most frequent ones
+        top_10_entries_spliceSeq = gene_counts_spliceSeq.head(10)
+        top_10_entries_splAdder = gene_counts_splAdder.head(10)
     else:
         gene_counts_spliceSeq = merged_df_spliceSeq.groupby(criterion1).size().sort_values(ascending=False)
         gene_counts_splAdder = merged_df_splAdder.groupby(criterion2).size().sort_values(ascending=False)   
 
-
-    # Select the 10 most frequent ones
-    top_10_entries_spliceSeq = gene_counts_spliceSeq.head(10)
-    top_10_entries_splAdder = gene_counts_splAdder.head(10)
-
     plt.figure(figsize=(10, 5))
 
     plt.subplot(1, 2, 1)
-    top_10_entries_spliceSeq.plot(kind='bar', color='navy')
+    if criterion2 == None:
+        top_10_entries_spliceSeq.plot(kind='bar', color='navy')
+    else:
+         gene_counts_spliceSeq.plot(kind='bar', color='navy')   
     plt.title(f'Top 10 Most Frequent {criterion1} (SpliceSeq)')
     plt.xlabel(criterion1)
     plt.ylabel('Number STR')
     plt.xticks(rotation=45)
-    
-    #for i, freq in enumerate(top_10_entries_spliceSeq):
-     #   plt.text(i, freq, str(freq), ha='center', va='bottom')
 
 
     plt.subplot(1, 2, 2)
-    top_10_entries_splAdder.plot(kind='bar', color='lightsteelblue')
+    if criterion2 == None:
+        top_10_entries_splAdder.plot(kind='bar', color='lightsteelblue')
+    else:
+        gene_counts_splAdder.plot(kind='bar', color='lightsteelblue')
     plt.title(f'Top 10 Most Frequent {criterion1} (SplAdder)')
     plt.xlabel(criterion1)
     plt.ylabel('Number STR')
     plt.xticks(rotation=45)
     
-    #for i, freq in enumerate(top_10_entries_splAdder):
-     #   plt.text(i, freq, str(freq), ha='center', va='bottom')
 
     plt.tight_layout()
     #plt.show()
-    #plt.savefig(f"data/comparison_{criterion1}_spliceSeq_splAdder.pdf")
+    plt.savefig(f"data/comparison_{criterion1}_spliceSeq_splAdder.pdf")
     
     
 if __name__=="__main__": 
